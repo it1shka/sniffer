6b74ed97f9f14fc343ff60029fd6bccf01431431
it1shka <2tishbel@gmail.com>
2024-03-26 12:27:16 +0100

    Added license

---
+++ web/__init__.py
@@ -0,0 +1,9 @@
+'''
+This package is responsible
+for running sniffer in browser
+'''
+
+
+def run_browser() -> None:
+    '''Will run browser version'''
+    raise Exception('Unimplemented functionality')

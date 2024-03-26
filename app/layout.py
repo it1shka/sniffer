8dc901d336ead876c5b5bafef4d017f68e2b9872
it1shka <2tishbel@gmail.com>
2024-03-26 10:04:47 +0100

    Created first version of application

--- app/layout.py
+++ app/layout.py
@@ -26,6 +26,7 @@ class AppLayout:
     start_button: ttk.Button
     stop_button: ttk.Button
     toggle_button: ttk.Button
+    clear_button: ttk.Button
     status_label: ttk.Label
     packets_list: ttk.Treeview
 
@@ -44,7 +45,13 @@ class AppLayout:
         self.start_button = ttk.Button(controls_frame, text='Start')
         self.stop_button = ttk.Button(controls_frame, text='Stop')
         self.toggle_button = ttk.Button(controls_frame, text='Toggle')
-        btns = [self.start_button, self.stop_button, self.toggle_button]
+        self.clear_button = ttk.Button(controls_frame, text='Clear')
+        btns = [
+            self.start_button,
+            self.stop_button,
+            self.toggle_button,
+            self.clear_button,
+        ]
         for button in btns:
             button.pack(side=tk.LEFT)
         self.status_label = ttk.Label(controls_frame, text='Off')
@@ -52,4 +59,16 @@ class AppLayout:
         controls_frame.pack(side=tk.TOP, fill='x')
 
     def _build_packets_list(self) -> None:
-        ...
+        columns = ('Value', )
+        self.packets_list = ttk.Treeview(self.window, columns=columns)
+        for column in columns:
+            self.packets_list.heading(column, text=column)
+        self.packets_list.pack(side=tk.LEFT, fill='both', expand=True)
+        scrollbar = ttk.Scrollbar(
+            self.window,
+            orient=tk.VERTICAL,
+            command=self.packets_list.yview,
+        )
+        self.packets_list.config(yscrollcommand=scrollbar.set)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.packets_list.config(padding=20)
--- app/logic.py
+++ app/logic.py
@@ -26,6 +26,7 @@ class AppLogic:
         self._layout.start_button.config(command=self._handle_start)
         self._layout.stop_button.config(command=self._handle_stop)
         self._layout.toggle_button.config(command=self._handle_toggle)
+        self._layout.clear_button.config(command=self._handle_clear)
 
     def run(self) -> None:
         '''Calls tkinter mainloop'''
@@ -40,7 +41,7 @@ class AppLogic:
     def _handle_stop(self) -> None:
         if not self._sniffer.running:
             return
-        self._sniffer.stop()
+        self._sniffer.stop(join=False)
         self._layout.status_label.config(text='Off')
 
     def _handle_toggle(self) -> None:
@@ -49,6 +50,21 @@ class AppLogic:
         else:
             self._handle_start()
 
+    def _handle_clear(self) -> None:
+        tree = self._layout.packets_list
+        tree.delete(*tree.get_children())
+
     def _process_packet(self, packet: Packet) -> None:
         '''Processes packets from AsyncSniffer'''
-        ...
+        last_layer_name = packet.layers()[-1].__name__
+        packet_title = f'{last_layer_name} packet'
+        root = self._layout.packets_list.insert('', 'end', text=packet_title)
+        self._insert_layers(root, packet)
+
+    def _insert_layers(self, root: str, packet: Packet) -> None:
+        for layer in packet.layers():
+            title = layer.__name__
+            layer_root = self._layout.packets_list.insert(root, 'end', text=title)
+            for field_name in map(lambda e: e.name, layer.fields_desc):
+                field_value = getattr(packet, field_name)
+                self._layout.packets_list.insert(layer_root, 'end', text=field_name, values=(field_value,))
